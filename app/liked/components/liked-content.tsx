66f8edaa6f77844f773da908ebaa28805fb9dde0
Aiden Schulman <aidenhschulman@gmail.com>
2023-09-01 13:43:30 -0400

    added liked songs functionality

--- app/liked/components/liked-content.tsx
+++ app/liked/components/liked-content.tsx
@@ -1,23 +1,37 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 import LikeButton from "@/components/like-button";
 import MediaItem from "@/components/media-item";
+import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
 
-interface SearchContentProps {
+interface LikedContentProps {
   songs: Song[];
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-        No songs found.
+        No liked songs
       </div>
     );
   }
+
   return (
-    <div className="flex flex-col gap-y-2 w-full px-6">
+    <div className="flex flex-col gap-y-2 w-full p-6">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
@@ -30,4 +44,4 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
   );
 };
 
-export default SearchContent;
+export default LikedContent;
